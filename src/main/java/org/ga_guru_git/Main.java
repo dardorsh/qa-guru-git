41778a406e4d069e06e2fce35a662e07813bbb1d
dardorsh <65073890+dardorsh@users.noreply.github.com>
2024-11-22 16:39:45 +0300

    test commit

---
+++ src/main/java/org/ga_guru_git/Main.java
@@ -0,0 +1,9 @@
+package org.ga_guru_git;
+
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++) {
+            System.out.println("Hello world " + i + "!");
+        }
+    }
+}
