30dba9ba8916eb9b93807872ef008a5771c52ba5
dardorsh <65073890+dardorsh@users.noreply.github.com>
2024-11-22 16:45:35 +0300

    change parameter

--- src/main/java/org/ga_guru_git/Main.java
+++ src/main/java/org/ga_guru_git/Main.java
@@ -2,7 +2,7 @@ package org.ga_guru_git;
 
 public class Main {
     public static void main(String[] args) {
-        for(int i = 1; i <= 10; i++) {
+        for(int i = 1; i < 11; i++) {
             System.out.println("Hello world " + i + "!");
         }
     }
